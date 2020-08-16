1d8afaea6d855ee21518cee55a4450f945b05b96
muhammadjabir <muhammadjabir525@gmail.com>
2020-08-16 19:07:41 +0700

    perbaikan api

--- resources/js/plugins/axios.js
+++ resources/js/plugins/axios.js
@@ -9,7 +9,7 @@ import axios from "axios";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-   baseURL:  "https://development.codhunter.academy/api"
+   baseURL:  "https://development.codehunter.academy/api"
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
