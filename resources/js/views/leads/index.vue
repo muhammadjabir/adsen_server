bd9c59e049800b617489e34fb70375491037311c
muhammadjabir <muhammadjabir525@gmail.com>
2020-09-09 11:28:37 +0700

    websockets frontend force

--- resources/js/views/leads/index.vue
+++ resources/js/views/leads/index.vue
@@ -441,7 +441,7 @@ export default {
         this.getKelas()
          var pusher = new Pusher('fdsfs23424rf', {
         cluster: 'mt1',
-        // forceTLS: true,
+        forceTLS: true,
         broadcaster: 'pusher',
         // key: 'fdsfs23424rf',
         wsHost: window.location.hostname,
