a0e68209feab2684c18047026d03ac2d95d85e8f
muhammadjabir <muhammadjabir525@gmail.com>
2020-12-03 09:21:03 +0700

    penambahan gitignore

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from './stores'
 import './plugins/axios.js'
 import vuetify from './plugins/vuetify'
+import { TiptapVuetifyPlugin } from 'tiptap-vuetify'
+import 'tiptap-vuetify/dist/main.css'
 // import CKEditor from '@ckeditor/ckeditor5-vue';
 // ck editor
 
@@ -16,3 +18,9 @@ new Vue({
    store,
   render: h => h(App)
 }).$mount('#app')
+Vue.use(TiptapVuetifyPlugin, {
+  // the next line is important! You need to provide the Vuetify Object to this place.
+  vuetify, // same as "vuetify: vuetify"
+  // optional, default to 'md' (default vuetify icons before v2.0.0)
+  iconsGroup: 'md'
+})
